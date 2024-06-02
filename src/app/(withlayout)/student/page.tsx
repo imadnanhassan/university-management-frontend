f0bf30ed6554ea5263f4d85fe41f7687fd0cf171
imadnanhassan <abulhassan.dev@gmail.com>
2024-06-02 12:18:19 +0600

    Configure Ant Design And Basic Routing

---
+++ src/app/(withlayout)/student/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StudentPage = () => {
+  return <div>This is Student page</div>;
+};
+
+export default StudentPage;
---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Button, Result } from "antd";
+import React from "react";
+
+const ErrorPage: React.FC = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={<Button type="primary">Back Home</Button>}
+    />
+  );
+};
+
+export default ErrorPage;
